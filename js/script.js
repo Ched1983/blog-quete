076b62b621a141e90364de7296fea2044dd1e035
ched <ched5@orange.fr>
2021-12-16 17:48:17 +0100

    jeudi 16/12

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const open1 = document.querySelector('#open1');
+const modal1 = document.querySelector('#modal1');
+const close1 = document.querySelector('#close1');
+
+open1.addEventListener('click', function() {
+    modal1.style.display = "block"
+})
+
+close1.addEventListener('click', function() {
+    modal1.style.display = "none"
+}) 
+
+const open2 = document.querySelector('#open2');
+const modal2 = document.querySelector('#modal2');
+const close2 = document.querySelector('#close2');
+
+open2.addEventListener('click', function() {
+    modal2.style.display = "block"
+})
+
+close2.addEventListener('click', function() {
+    modal2.style.display = "none"
+})
+
+const open3 = document.querySelector('#open3');
+const modal3 = document.querySelector('#modal3');
+const close3 = document.querySelector('#close3');
+
+open3.addEventListener('click', function() {
+    modal3.style.display = "block"
+})
+
+close3.addEventListener('click', function() {
+    modal3.style.display = "none"
+})
